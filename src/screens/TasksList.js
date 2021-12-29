0e8e2d234a7303c05050a9ad84291b2c0613fe38
Mateus Leonardo <m4teusmatos@gmail.com>
2021-12-29 14:18:00 -0300

    :art: add bg the tasks list

--- src/screens/TasksList.js
+++ src/screens/TasksList.js
@@ -1,12 +1,24 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, ImageBackground, StyleSheet} from 'react-native';
+
+import todayImage from '../../assets/imgs/today.jpg';
 
 const TasksList = () => {
   return (
-    <View>
+    <View style={styles.container}>
+      <ImageBackground source={todayImage} style={styles.background} />
       <Text style={{color: '#fff'}}>TasksList</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  background: {
+    flex: 1,
+  },
+});
 
 export default TasksList;
