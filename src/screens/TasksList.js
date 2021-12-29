c4fa37e3ce7ae11d30efe91c48fbfdd0c6fb8024
Mateus Leonardo <m4teusmatos@gmail.com>
2021-12-29 10:22:40 -0300

    :rocket: creating the screen task list

---
+++ src/screens/TasksList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const TasksList = () => {
+  return (
+    <View>
+      <Text style={{color: '#fff'}}>TasksList</Text>
+    </View>
+  );
+};
+
+export default TasksList;
