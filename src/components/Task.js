69f57ae9976a0fbb9d78a85d0700855e8085fef9
Mateus Leonardo <m4teusmatos@gmail.com>
2021-12-30 15:28:33 -0300

    :art: setting layout the check

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
+import Types from '../Types';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+function getCheckView(doneAt) {
+  if (doneAt != null) {
+    return (
+      <View style={styles.done}>
+        <Icon name="check" size={20} color="#FFF" />
+      </View>
+    );
+  } else {
+    return <View style={styles.pending} />;
+  }
+}
 
 const Task = props => {
   return (
     <View style={styles.container}>
-      <Text style={{color: '#fafafa'}}>{props.desc}</Text>
-      <Text style={{color: '#fafafa'}}>{props.estimateAt + ''}</Text>
-      <Text style={{color: '#fafafa'}}>{props.doneAt + ''}</Text>
+      <View style={styles.checkContainer}>{getCheckView(props.doneAt)}</View>
+      <View>
+        <Text>{props.desc}</Text>
+        <Text>{props.estimateAt + ''}</Text>
+      </View>
     </View>
   );
 };
@@ -14,6 +30,29 @@ const Task = props => {
 const styles = StyleSheet.create({
   container: {
     flexDirection: 'row',
+    borderColor: '#AAA',
+    borderBottomWidth: 1,
+    alignItems: 'center',
+    paddingVertical: 10,
+  },
+  checkContainer: {
+    width: '20%',
+    alignItems: 'center',
+  },
+  pending: {
+    width: 25,
+    height: 25,
+    borderRadius: 13,
+    borderWidth: 1,
+    borderColor: '#555',
+  },
+  done: {
+    width: 25,
+    height: 25,
+    borderRadius: 13,
+    backgroundColor: '#4D7031',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
 
