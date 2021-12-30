c7631747aad6ea7fed1de470004ab32966c4654f
Mateus Leonardo <m4teusmatos@gmail.com>
2021-12-30 08:56:12 -0300

    :rocket: creating file the variables

--- src/screens/TasksList.js
+++ src/screens/TasksList.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import {View, Text, ImageBackground, StyleSheet} from 'react-native';
 
+import Types from '../Types';
 import todayImage from '../../assets/imgs/today.jpg';
+import moment from 'moment';
+import 'moment/locale/pt-br';
 
 const TasksList = () => {
+  const today = moment().locale('pt-br').format('ddd, D [de] MMMM');
   return (
     <View style={styles.container}>
-      <ImageBackground source={todayImage} style={styles.background} />
-      <Text style={{color: '#fff'}}>TasksList</Text>
+      <ImageBackground source={todayImage} style={styles.background}>
+        <View style={styles.titleBar}>
+          <Text style={styles.title}>Hoje</Text>
+          <Text>{today}</Text>
+        </View>
+      </ImageBackground>
+      <View style={styles.taskList}>
+        <Text style={{color: '#fff'}}>TasksList</Text>
+      </View>
     </View>
   );
 };
@@ -17,7 +28,18 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   background: {
+    flex: 3,
+  },
+  taskList: {
+    flex: 7,
+  },
+  titleBar: {
     flex: 1,
+    justifyContent: 'flex-end',
+  },
+  title: {
+    fontFamily: Types.fontFamily,
+    fontSize: 50,
   },
 });
 
